b5b45c5dc915197a76d6e181660d671bf86adc5c
Y.Y <yagi.yoshiaki04@gmail.com>
2023-03-02 02:31:37 +0900

    set navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { NavBarComponent } from './landing/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    TopMenuComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      { path:'',component: LandingComponent}
+      { path:'/landing',component: LandingComponent},
+      { path:'/top',component: TopMenuComponent},
+      { path:'/navbar',component: NavBarComponent}
     ])
   ],
   providers: [],
